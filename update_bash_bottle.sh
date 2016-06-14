8f063c3c6ffd8e0e2d488a225aed226b046545ff
Joseph Gooch <mrwizard@k12system.com>
2016-06-14 13:16:27 -0500

    bottle update script mod

--- update_bash_bottle.sh
+++ update_bash_bottle.sh
@@ -1,10 +1,13 @@
+export HOMEBREW_DEVELOPER=1
 brew test-bot bash
 for i in bash*.bottle*.tar.gz; do
   brew install ./$i
 done
-brew bottle --rb --root_url=http://debian.dok.org/homebrew/ bash
-for i in bash-*.bottle.rb; do
-  brew bottle --write --merge $i
-done
+brew bottle --rb --root_url=http://debian.dok.org/homebrew/ --revision=2 bash
+#for i in bash-*.bottle.rb; do
+#  brew bottle --write --keep-old $i
+#done
 scp bash-*.bottle*tar.gz dok_debian@debian.dok.org:~/debian.dok.org/homebrew/
+
+git push dok master:wizmods
 
